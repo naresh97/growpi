9912fe7b1397ea1d7c239cf11c05a0156aefa217
Nareshkumar Rao <629990+naresh97@users.noreply.github.com>
2024-05-08 13:27:03 +0200

    bugfix with light control

--- src/control/light.rs
+++ src/control/light.rs
@@ -10,7 +10,7 @@ use crate::{
 
 fn should_turn_on_light(on_hours: u64, lights_out: u64, current_hour: u64) -> bool {
     let off_hours = 24 - on_hours;
-    (lights_out..(lights_out + off_hours))
+    !(lights_out..(lights_out + off_hours))
         .map(|x| x % 24)
         .any(|x| x == current_hour)
 }
@@ -22,7 +22,6 @@ fn light_control(program_state: ProgramStateShared) -> GenericResult<()> {
     let on_hours = program_state.config.controller_settings.sunlight_hours;
     let current_hour = Local::now().time().hour() as u64;
     let lights_out_hour = program_state.config.controller_settings.lights_off_hour;
-
     if should_turn_on_light(on_hours, lights_out_hour, current_hour) {
         actuators::switch_lights(crate::io::RelaySwitchState::On, &mut program_state)?;
     } else {
@@ -44,25 +43,27 @@ mod tests {
     use super::*;
     #[test]
     fn test_should_turn_on_light() {
-        assert!(!should_turn_on_light(24, 5, 4));
-        assert!(!should_turn_on_light(24, 5, 5));
-        assert!(!should_turn_on_light(24, 5, 6));
-        assert!(!should_turn_on_light(24, 0, 0));
-        assert!(!should_turn_on_light(24, 0, 23));
-        assert!(!should_turn_on_light(24, 0, 1));
+        assert!(should_turn_on_light(24, 5, 4));
+        assert!(should_turn_on_light(24, 5, 5));
+        assert!(should_turn_on_light(24, 5, 6));
+        assert!(should_turn_on_light(24, 0, 0));
+        assert!(should_turn_on_light(24, 0, 23));
+        assert!(should_turn_on_light(24, 0, 1));
 
-        assert!(!should_turn_on_light(23, 5, 4));
-        assert!(should_turn_on_light(23, 5, 5));
-        assert!(!should_turn_on_light(23, 5, 6));
-        assert!(!should_turn_on_light(23, 0, 23));
-        assert!(should_turn_on_light(23, 0, 0));
-        assert!(!should_turn_on_light(23, 0, 1));
+        assert!(should_turn_on_light(23, 5, 4));
+        assert!(!should_turn_on_light(23, 5, 5));
+        assert!(should_turn_on_light(23, 5, 6));
+        assert!(should_turn_on_light(23, 0, 23));
+        assert!(!should_turn_on_light(23, 0, 0));
+        assert!(should_turn_on_light(23, 0, 1));
 
-        assert!(!should_turn_on_light(20, 22, 21));
-        assert!(should_turn_on_light(20, 22, 22));
-        assert!(should_turn_on_light(20, 22, 23));
-        assert!(should_turn_on_light(20, 22, 0));
-        assert!(should_turn_on_light(20, 22, 1));
-        assert!(!should_turn_on_light(20, 22, 2));
+        assert!(should_turn_on_light(20, 22, 21));
+        assert!(!should_turn_on_light(20, 22, 22));
+        assert!(!should_turn_on_light(20, 22, 23));
+        assert!(!should_turn_on_light(20, 22, 0));
+        assert!(!should_turn_on_light(20, 22, 1));
+        assert!(should_turn_on_light(20, 22, 2));
+
+        assert!(should_turn_on_light(24, 0, 13));
     }
 }
